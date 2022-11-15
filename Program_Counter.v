8534f8f524dee3c8f4ac1a1a41411910110839e5
kimseojin <95688563+loltochess@users.noreply.github.com>
2022-11-15 17:02:47 +0900

    Update Program_Counter.v

--- Program_Counter.v
+++ Program_Counter.v
@@ -7,28 +7,25 @@ module Program_Counter(CLK,PL,JB,BC,LAddress,RAddress,AData,PC);
     reg [3:0] PC;
 
     initial begin
-        LAddress<=2'b00;
-        RAddress<=2'b00;
-        AData<=4'b0000;
-        PC<=4'b0000;
+        PC<=4'b0101;
     end
 
     always@(posedge CLK) begin
         if(~PL)
-            PC<=PC+4'b0100;
+            PC<=PC+1;
         else if(JB)
             PC<=AData;
         else if(BC) begin
             if(AData<4'b0000)
                 PC<=PC+{LAddress[1:0],RAddress[1:0]}
             else
-                //
+                PC<=PC+1;
         end
         else if(~BC) begin
             if(AData==4'b0000)
                 PC<=PC+{LAddress[1:0],RAddress[1:0]};
             else
-            //
+                PC<=PC+1;
         end
     end
 endmodule
